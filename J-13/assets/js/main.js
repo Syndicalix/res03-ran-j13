42328b6bc880101a2125a1fe70a143e8f6f8fc86
Syndicalix <sebastienspeich@ip-172-31-28-163.eu-west-3.compute.internal>
2022-11-24 11:30:59 +0000

    git push

--- J-13/assets/js/main.js
+++ J-13/assets/js/main.js
@@ -1,3 +1,12 @@
 window.addEventListener("DOMContentLoaded", function(event){
 
 });
+
+function getNextSection(current)
+{
+    if(currend.id === "red")
+    {return document.getElementById("orange")
+}
+}
+let sections=document.querySelectorAll("body > main > section")
+let currentScrollPosition = document.documentElement.scrollTop;
