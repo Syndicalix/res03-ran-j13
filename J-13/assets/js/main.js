556d4b030f7f2eb6a510d4234913708084aa33ed
Syndicalix <sebastienspeich@ip-172-31-28-163.eu-west-3.compute.internal>
2022-11-24 15:25:35 +0000

    add commit push

--- J-13/assets/js/main.js
+++ J-13/assets/js/main.js
@@ -7,7 +7,17 @@ function getNextSection(current)
     if(currend.id === "red")
     {return document.getElementById("orange")
 }
+    if(currend.id ==="orange")
+    {return document.getElementById("yellow")
 }
+    if (currend.id ==="yellow")
+    {return document.getElementById("green")
+    }    
+    if (currend.id ==="green")
+    {return document.getElementById("blue")
+    }
+    if(currend.id ==="blue")
+    {return document.getElementById("purple")
 
 let sections=document.querySelectorAll("body > main > section")
 let currentScrollPosition = document.documentElement.scrollTop;
